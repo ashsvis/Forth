7f8c9b79ab309f2db3e6d713329e13fb8b86cd64
Александр Шаповалов <ashsvis@gmail.com>
2025-09-02 17:57:47 +0300

    встроенные слова dup, drop, swap, over, rot

--- ForthEditor/ForthModel.cs
+++ ForthEditor/ForthModel.cs
@@ -6,6 +6,80 @@ namespace ForthEditor
     {
         private string commandLine = string.Empty;
 
+        private readonly Dictionary<string, Action<ObservableCollection<StackLine>>> words = new() { 
+            { "+", (stack) => 
+                { 
+                    var value2 = stack[0].Value;
+                    stack.RemoveAt(0);
+                    var value1 = stack[0].Value;
+                    stack.RemoveAt(0);
+                    stack.Insert(0, new StackLine(value1 + value2));
+                } 
+            },
+            { "-", (stack) =>
+                {
+                    var value2 = stack[0].Value;
+                    stack.RemoveAt(0);
+                    var value1 = stack[0].Value;
+                    stack.RemoveAt(0);
+                    stack.Insert(0, new StackLine(value1 - value2));
+                }
+            },
+            { "*", (stack) =>
+                {
+                    var value2 = stack[0].Value;
+                    stack.RemoveAt(0);
+                    var value1 = stack[0].Value;
+                    stack.RemoveAt(0);
+                    stack.Insert(0, new StackLine(value1 * value2));
+                }
+            },
+            { "/", (stack) =>
+                {
+                    var value2 = stack[0].Value;
+                    stack.RemoveAt(0);
+                    var value1 = stack[0].Value;
+                    stack.RemoveAt(0);
+                    stack.Insert(0, new StackLine(value1 / value2));
+                }
+            },
+            { "dup", (stack) =>
+                {
+                    // дублирует верхний элемент стека
+                    var value = stack[0].Value;
+                    stack.Insert(0, new StackLine(value));
+                }
+            },
+            { "drop", (stack) =>
+                {
+                    // удаляет верхний элемент стека
+                    stack.RemoveAt(0);
+                }
+            },
+            { "swap", (stack) =>
+                {
+                    // меняет местами два верхних элемента стека
+                    var value = stack[1].Value;
+                    stack.RemoveAt(1);
+                    stack.Insert(0, new StackLine(value));
+                }
+            },
+            { "over", (stack) =>
+                {
+                    // берет второй элемент с вершины стека и дублирует его на вершину стека
+                    var value = stack[1].Value;
+                    stack.Insert(0, new StackLine(value));
+                }
+            },
+            { "rot", (stack) =>
+                {
+                    // третий элемент с вершины стека перемещается на вершину стека, вытесняя два других элемента вниз
+                    var value = stack[2].Value;
+                    stack.RemoveAt(2);
+                    stack.Insert(0, new StackLine(value));
+                }
+            },
+        };
 
         public ForthModel() { }
 
@@ -24,16 +98,32 @@ namespace ForthEditor
 
         public void EnterCommand(string text)
         {
-            if (double.TryParse(text, System.Globalization.NumberStyles.Float, 
-                System.Globalization.CultureInfo.GetCultureInfo("en-US"),  out double number))
+            var enteredWords = text.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            foreach (var word in enteredWords)
             {
-                StackLines.Insert(0, new StackLine(number));
-                CommandLines.Add(new CommandLine(text));
-                CommandLines.Add(new CommandLine("ok"));
-            }
-            else
-            {
-                CommandLines.Add(new CommandLine(text + "?"));
+                if (double.TryParse(word, System.Globalization.NumberStyles.Float,
+                    System.Globalization.CultureInfo.GetCultureInfo("en-US"), out double number))
+                {
+                    StackLines.Insert(0, new StackLine(number));
+                    CommandLines.Add(new CommandLine(word + " ok"));
+                }
+                else if (words.TryGetValue(word, out Action<ObservableCollection<StackLine>>? action))
+                {
+                    try
+                    {
+                        action(StackLines);
+                    }
+                    catch (Exception ex)
+                    {
+                        StackLines.Clear();
+                        CommandLines.Add(new CommandLine(ex.Message));
+                    }
+                }
+                else
+                {
+                    CommandLines.Add(new CommandLine(text + "?"));
+                    break;
+                }
             }
             CommandLine = string.Empty;
         }
